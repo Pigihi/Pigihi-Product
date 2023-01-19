8435d05587c38ec51578055c8bfc3021b5690b69
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2023-01-19 09:21:54 +0530

    Setup project structure

---
+++ src/main/java/com/pigihi/PigihiProductApplication.java
@@ -0,0 +1,13 @@
+package com.pigihi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigihiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigihiProductApplication.class, args);
+	}
+
+}
